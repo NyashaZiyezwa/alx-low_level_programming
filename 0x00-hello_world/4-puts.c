b3f3015939a75ae9691cff353bb3146bddd1e63b
NyashaZiyezwa <nziyezwa@gmail.com>
2022-09-12 03:35:21 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -11,7 +10,7 @@
 
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return 0;
+	puts(" \ "programming is like building a multilingual puzzle");
+	return (0);
 }
 
